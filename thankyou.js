6a67d13dbd80a0060b6d671dfc0e78d5d4eeedb8
Aviral Ale <aviralale@gmail.com>
2023-04-30 15:24:58 +0545

    responsive thankyou page

--- thankyou.js
+++ thankyou.js
@@ -1,6 +1,6 @@
-setTimeout(function() {
-    window.location.href = "../index.html";
-  }, 6000);
+// setTimeout(function() {
+//     window.location.href = "../index.html";
+//   }, 6000);
 
   let count = 5;
 const counter = setInterval(() => {
